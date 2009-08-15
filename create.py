78bac4c53310015036980fc7ab4638fd732105b6
James Cash <james.nvc@gmail.com>
2009-08-15 14:16:44 -0400

    Verifying user checking, starting to add user creation

--- create.py
+++ create.py
@@ -1,21 +1,7 @@
-#!/usr/bin/env python2.5
+# Add a new user
 import xmlrpclib
 import cgi
 import cgitb
 cgitb.enable()
 
-form = cgi.FieldStorage()
-name = form.getvalue('new_username')
 
-auth = xmlrpclib.ServerProxy('http://localhost:8082/auth')
-res = auth.userExists(name)
-
-print 'Content-type: text/html'
-print
-print '<html><head><title>User exists checking</title></head>'
-print '<body>'
-if res:
-    print "<p>User exists: uid = %d</p>" % res
-else:
-    print '<p>No user by that name</p>'
-print '</body></html>'
