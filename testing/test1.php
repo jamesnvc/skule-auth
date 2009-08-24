57bc8e3aedb70aa863fff9f7fef05e2e02588083
James Cash <james@james-cashs-macbook-pro.local>
2009-08-24 08:02:09 -0400

    Using BlueTrip CSS framework for testing pages

--- testing/test1.php
+++ testing/test1.php
@@ -4,30 +4,32 @@ unset($_SESSION['rand_val']);
 $_SESSION['rand_val'] = rand();
 ?>
 <html>
-<head><title>Test 1</title></head>
+<head>
+  <title>Test 1</title>
+  <link rel="stylesheet" href="/~james/css/bluetrip/screen.css" type="text/css" media="screen, projection"/>
+  <link rel="stylesheet" href="/~james/css/bluetrip/print.css" type="text/css" media="print"/> 
+  <!--[if IE]>
+      <link rel="stylesheet" href="/~james/css/bluetrip/ie.css" type="text/css" media="screen, projection"/>
+      <![endif]-->
+  <link rel="stylesheet" href="/~james/css/bluetrip/style.css" type="text/css" media="screen, projection"/>
+</head>
 <body>
-<pre>
-<?php
-print_r($_COOKIE);
-print_r($_SESSION);
-?>
-</pre>
-<p>Test One:
-<?php if (isset($_COOKIE['username'])) { ?>
+  <div class="container">
+  <h1>Test One:</h1>
+  <?php if (isset($_COOKIE['username'])) { ?>
    <p>Hello, <? echo $_COOKIE['username'] ?>!</p>
    <p><form action="logout.php" method="POST"><input type="submit" value="Logout"/></form></p>
 <?php    } else { ?>
    <p>Please <a href="./login.html">Login</a></p>
      <?php } ?>
-<?php echo time() ?>     
+<h2>Enter Something</h2>
 <form action="test2.php" method="POST">
-   <label title="A string" for="foo">String:
-      <input type="text" name="foo" />
-   </label>
+   <label title="A string" for="foo">String:</label>
+   <input type="text" name="foo" />
    <input type="hidden" name="bar" value="<?php echo hash('sha256', $_COOKIE['sid'] . $_SESSION['rand_val'])?>"/>
-   <input type="hidden" name="phpses" value="<?php echo session_id(); ?>">
+   <input type="hidden" name="phpses" value="<?php echo session_id(); ?>"/>
    <input type="submit" value="done" />
-</form>
-</p>
+ </form>
+</div>
 </body>
 </html>
--- testing/test2.php
+++ testing/test2.php
@@ -3,22 +3,24 @@ session_id($_POST['phpses']);
 session_start();
 ?>
 <html>
-<head><title>Test 2</title></head>
+<head>
+<title>Test 2</title>
+  <link rel="stylesheet" href="/~james/css/bluetrip/screen.css" type="text/css" media="screen, projection"/>
+  <link rel="stylesheet" href="/~james/css/bluetrip/print.css" type="text/css" media="print"/> 
+  <!--[if IE]>
+      <link rel="stylesheet" href="/~james/css/bluetrip/ie.css" type="text/css" media="screen, projection"/>
+      <![endif]-->
+  <link rel="stylesheet" href="/~james/css/bluetrip/style.css" type="text/css" media="screen, projection"/>
+</head>
 <body>
-<pre>
-<?php
-echo 'COOKIE:';
-print_r($_COOKIE);
-echo 'SESSION:';
-print_r($_SESSION);
-?>
-</pre>
+<div class="container">
 <?php if ( isset($_COOKIE['sid']) && $_POST['bar'] == hash('sha256', $_COOKIE['sid'] . $_SESSION['rand_val'])) { ?>
 <p>You entered <? echo $_POST['foo'] ?></p>
 <?php } else { ?>
 <p>Authentication error!</p>
 <p><a href="./test1.php">Try again?</a></p>
 <?php } ?>
+</div>
 </body>
 </html>
 <?php
