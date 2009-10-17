f63ca255ba43f28773e30971fdaf9a5b6e9b6fd2
James Cash <james@user132-200.wireless.utoronto.ca>
2009-10-17 14:56:25 -0400

    Not inserting default users into db

--- testing.sql
+++ testing.sql
@@ -6,10 +6,3 @@ firstname varchar(100),
 lastname varchar(100),
 salt varchar(10)
 );
-
--- Password is 'aaa'
-INSERT INTO user VALUES (1, 'admin', 'ab5OR/l8cWmIc', 'Admin', 'User');
--- Password is 'bbb'
-INSERT INTO user VALUES (2, 'test1', 'abjKJAkaL5HaE', 'Joe', 'Smith');
--- Password is 'ccc'
-INSERT INTO user VALUES (3, 'test2', 'abmPzTXULWrKw', 'Bob', 'King');
